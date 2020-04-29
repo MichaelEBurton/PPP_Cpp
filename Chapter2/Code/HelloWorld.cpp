0c87b17b7ff0b92048dcb77ae84570c94cb0ce15
meburton19 <52686856+meburton19@users.noreply.github.com>
2020-04-29 13:09:47 -0400

    Ahh

--- Chapter2/Code/HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-// This program outputs the message "Hello World!" to the montor
-
-#include "../../../std_lib_facilities.h"
-
-int main()        // C++ programs start by executing the function main
-{
-    cout<<"Hellow, World!\n"; // output "Hello, World!"
-    return 0;
-}
