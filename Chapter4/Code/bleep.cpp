8d9440fcc96b1ae5b87320a6246ffed7822f06e1
meburton19 <52686856+meburton19@users.noreply.github.com>
2020-05-18 12:42:17 -0400

    Using functions to check for disliked words

--- Chapter4/Code/bleep.cpp
+++ Chapter4/Code/bleep.cpp
@@ -2,19 +2,33 @@
 //Will not run on git bash
 #include "../../../std_lib_facilities.h"
 
+bool bleep(vector<string> bad_words, string word)
+{
+    for(int i =0; i < bad_words.size();++i){
+        if(word == bad_words[i]) return true;
+    }
+    return false;
+}
+
 int main()
 {
-    string disliked = "Broccoli";
-
+    vector<string>disliked = {"Broccoli", "Pear", "Banana", "Tomato"};
     vector<string> words;
+
+    cout<<"Please enter your favorite words:\n";
     for(string i; cin>>i;) //read
         words.push_back(i); //put into vector
+
     cout<<"Number of words: "<<words.size()<<"\n";
 
+    int c = 0;
     for(int i=0; i < words.size(); ++i){
-        if(words[i] != disliked)
-            cout<<words[i]<<"\n";
-        else
+
+        if(bleep(disliked, words[i])){
             cout<<"BLEEP"<<"\n";
+        } else
+        {
+            cout<<words[i]<<"\n";
+        }
     }
 }
