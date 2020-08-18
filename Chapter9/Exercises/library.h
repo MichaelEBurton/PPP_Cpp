c463ce276a61d9e939786031d2d2c6097c5045da
meburton19 <52686856+meburton19@users.noreply.github.com>
2020-08-18 14:50:54 -0400

    Welp

---
+++ Chapter9/Exercises/library.h
@@ -0,0 +1,34 @@
+#include "book.h"              //Import book class
+#include "patron.h"            //Import Patron class
+#include "../code/Chrono.h"    //Import Date class
+
+namespace Library_sys {
+
+struct Transaction 
+{
+    Book b;
+    Patron p;
+    Chrono::Date d;
+    Transaction(Book bb, Patron pp, Chrono::Date dd)
+        :b{bb}, p{pp}, d{dd} {}
+};
+
+class Library 
+{
+public:
+    void add_book(Book b) {Books.push_back(b);  }                 // Add a book to the library
+    void add_Patron(Patron p) {Patrons.push_back(p);}             // Add a Patron to the library
+    void checkout_book(Patron p, Book b, Chrono::Date d);                 // Checkout a book
+    vector<Patron> fees_list();                                  //List of Patrons that owe fees
+
+private:
+   vector<Book> Books;
+   vector<Patron> Patrons;
+   vector<Transaction> transactions;
+
+   bool is_patron(Patron p);
+   bool book_exists(Book b);
+
+};
+
+}
